9d2959b0c6a057697c24de4e10dc4facb7c8128d
cucumber007 <cucumber007@mail.ru>
2016-08-12 13:40:45 +0300

    Permissions

--- app/src/main/java/com/cucumber007/prototypes/activities/_libraries/reactive_location/ReactiveLocationActivity.java
+++ app/src/main/java/com/cucumber007/prototypes/activities/_libraries/reactive_location/ReactiveLocationActivity.java
@@ -1,12 +1,17 @@
 package com.cucumber007.prototypes.activities._libraries.reactive_location;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.cucumber007.prototypes.R;
+import com.cucumber007.prototypes.utils.LogUtil;
 import com.google.android.gms.location.LocationRequest;
 
 import java.util.List;
@@ -27,6 +32,7 @@ public class ReactiveLocationActivity extends AppCompatActivity {
 
     private Context context = this;
     private Subscription subscription;
+    public static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,64 @@ public class ReactiveLocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reactive_location);
         ButterKnife.bind(this);
 
+        LogUtil.logDebug("test");
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            LogUtil.logDebug("already granted");
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    LogUtil.logDebug("ok");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @OnClick(R.id.button14)
@@ -74,7 +137,7 @@ public class ReactiveLocationActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
-        subscription.unsubscribe();
+        if (subscription != null) subscription.unsubscribe();
         super.onStop();
     }
 }
