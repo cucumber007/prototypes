c8a277526f9a7ff81549d22fd1f80d18bbceee41
cucumber007 <cucumber007@mail.ru>
2016-08-15 15:02:05 +0300

    OkHttp cache

--- app/src/main/java/com/cucumber007/prototypes/activities/_libraries/retrofit/RetrofitActivity.java
+++ app/src/main/java/com/cucumber007/prototypes/activities/_libraries/retrofit/RetrofitActivity.java
@@ -1,13 +1,14 @@
 package com.cucumber007.prototypes.activities._libraries.retrofit;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.cucumber007.prototypes.R;
 
-import java.io.IOException;
+import java.io.File;
 
-import okhttp3.Interceptor;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -16,6 +17,8 @@ import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
 
 public class RetrofitActivity extends Activity {
+
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +32,42 @@ public class RetrofitActivity extends Activity {
         httpClient.interceptors().add(logging);
 
         //cookie
-        httpClient.interceptors().add(new Interceptor() {
-            @Override
-            public Response intercept(Interceptor.Chain chain) throws IOException {
-                Request original = chain.request();
+        httpClient.interceptors().add(chain -> {
+            Request original = chain.request();
 
-                String cookie = null; // = CookieModel.getInstance().getCookie();
-                if (cookie == null) {
-                    cookie = "";
-                }
+            String cookie = null; // = CookieModel.getInstance().getCookie();
+            if (cookie == null) {
+                cookie = "";
+            }
 
-                Request.Builder requestBuilder = original.newBuilder()
-                        .header("Cookie", cookie)
-                        .method(original.method(), original.body());
+            Request.Builder requestBuilder = original.newBuilder()
+                    .header("Cookie", cookie)
+                    .method(original.method(), original.body());
 
-                Request request = requestBuilder.build();
-                return chain.proceed(request);
+            Request request = requestBuilder.build();
+            return chain.proceed(request);
+        });
+
+        OkHttpClient.Builder clientBuilder = new OkHttpClient().newBuilder();
+        clientBuilder.addNetworkInterceptor(chain -> {
+            Response originalResponse = chain.proceed(chain.request());
+            if (isNetworkAvailable()) {
+                int maxAge = 60; // read from cache for 1 minute
+                return originalResponse.newBuilder()
+                        .header("Cache-Control", "public, max-age=" + maxAge)
+                        .build();
+            } else {
+                int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
+                return originalResponse.newBuilder()
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                        .build();
             }
         });
+
+        File httpCacheDirectory = new File(context.getCacheDir(), "responses");
+        int cacheSize = 10 * 1024 * 1024; // 10 MiB
+        Cache cache = new Cache(httpCacheDirectory, cacheSize);
+        clientBuilder.cache(cache);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://cucumber007app.herokuapp.com")
@@ -55,4 +76,6 @@ public class RetrofitActivity extends Activity {
                 .build();
         RetrofitService service = retrofit.create(RetrofitService.class);
     }
+
+    private boolean isNetworkAvailable() {return  true;}
 }
