bb8d45ab284f650f8dcf6e6723fc4944758ed413
cucumber007 <cucumber007@mail.ru>
2017-02-26 15:06:26 +0200

    reusables

---
+++ app/src/main/java/com/cucumber007/prototypes/reusables/RetrofitService.java
@@ -0,0 +1,30 @@
+package com.cucumber007.prototypes.reusables;
+
+
+import okhttp3.RequestBody;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import rx.Observable;
+
+public interface RetrofitService {
+
+    @POST("login")
+    Observable<Response<Object>> login(@Body Object loginParams);
+
+    @GET("offers/{id}/")
+    Observable<Object> getOffers(@Query("latitude") double latitude, @Path("id") int offerId);
+
+    @Multipart
+    @POST("offers/{id}/finish_checkin")
+    Observable<Object> finishCheckin(
+            @Part("checkin_id") RequestBody checkinId,
+            @Part("file\"; filename=\"file.jpg\" ") RequestBody file
+    );
+
+}
