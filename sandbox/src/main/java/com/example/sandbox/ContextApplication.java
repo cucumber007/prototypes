ebbc77a9785383e7cbd6b4daa3a5b2241310fd34
cucumber007 <cucumber007@mail.ru>
2017-06-02 15:59:01 +0300

    Rotation sandbox

---
+++ sandbox/src/main/java/com/example/sandbox/ContextApplication.java
@@ -0,0 +1,22 @@
+package com.example.sandbox;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ContextApplication extends Application{
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+
+    public static Context getContext() {
+        return context;
+    }
+
+
+}
