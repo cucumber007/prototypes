d572d6c31dddaa18b9b429c7d16b47eead55c547
cucumber007 <cucumber007@mail.ru>
2017-10-14 15:08:25 +0300

    Copypaste

---
+++ copypaste/src/main/java/com/polyana/cucumber007/copypaste/activities/AbstractPopupActivity.java
@@ -0,0 +1,15 @@
+package com.polyana.cucumber007.copypaste.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.polyana.cucumber007.copypaste.R;
+
+public class AbstractPopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abstract_popup);
+    }
+}
