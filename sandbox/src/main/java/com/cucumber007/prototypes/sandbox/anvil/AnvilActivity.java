3245ec2f3b6de60369bb9e1d9b13c1af508c2fb3
Butterfork <butterfork@syneforge.com>
2018-03-09 11:45:56 +0200

    Rename

---
+++ sandbox/src/main/java/com/cucumber007/prototypes/sandbox/anvil/AnvilActivity.java
@@ -0,0 +1,15 @@
+package com.cucumber007.prototypes.sandbox.anvil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cucumber007.prototypes.R;
+
+public class AnvilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anvil);
+    }
+}
